8e2a619473544ac5ead35f7581d4b77eba0fa10e
Anik Saha <dev.oniksaha@gmail.com>
2026-07-23 17:50:04 +0600

    test(rectangle stripe painter): enhance tests for stripe coordinate calculations and ensure parallelism

--- test/rectangle_stripes_test.dart
+++ test/rectangle_stripes_test.dart
@@ -4,16 +4,38 @@ import 'package:inno_entry/src/feature/entry/presentation/widgets/rectangle_stri
 
 void main() {
   group('Rectangle stripe tests:', () {
-    test("Should return true", () {
-      // get stripe coordinates
-      final stripeCoordinates = RectangleStripePainter.stripes(size: Size(100, 100), gap: 5);
-      if(stripeCoordinates.isEmpty) return false;
-      final distanceVector = stripeCoordinates[0].top - stripeCoordinates[0].bottom;
-      for(int index = 0; index < stripeCoordinates.length; index++) {
-        final thisDistanceVector = stripeCoordinates[index].top - stripeCoordinates[index].bottom;
-        if(thisDistanceVector != distanceVector) return false;
+    // get stripe coordinates
+    final stripeCoordinates = RectangleStripePainter.stripes(size: Size(200000, 100), gap: 5);
+
+    test("List is not empty.", () {
+      expect(true, stripeCoordinates.isNotEmpty);
+    });
+
+    test("Different stripes!", () {
+      bool yes = true;
+      for(int index = 1; index < stripeCoordinates.length; index++) {
+        if(stripeCoordinates[index] == stripeCoordinates[index -1]) {
+          yes = false; break;
+        }
       }
-      return true;
+
+      expect(true, yes);
+    });
+
+    test("Stripes should be parallel to each other", () {
+      bool isParellerAndEqualGaps() {
+        if(stripeCoordinates.isEmpty) return false;
+        final slope = (stripeCoordinates[0].top.dy - stripeCoordinates[0].bottom.dy) / (stripeCoordinates[0].top.dx - stripeCoordinates[0].bottom.dx);
+          for(int index = 1; index < stripeCoordinates.length; index++) {
+            final thisSlope = (stripeCoordinates[0].top.dy - stripeCoordinates[0].bottom.dy) / (stripeCoordinates[0].top.dx - stripeCoordinates[0].bottom.dx);
+            if(thisSlope != slope) return false;
+            //if(thisDistanceVector != distanceVector) return false;
+          }
+          return true;
+      }
+
+      expect(true, isParellerAndEqualGaps());
+      
     });
   });
 }
