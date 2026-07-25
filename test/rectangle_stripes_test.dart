d9e5fe96c85bef60749750fa4ae8391de28a558e
Anik Saha <dev.oniksaha@gmail.com>
2026-07-25 11:41:00 +0600

    Add: added test logic for rectangle side gaps of the stripes coordinates.

--- test/rectangle_stripes_test.dart
+++ test/rectangle_stripes_test.dart
@@ -4,8 +4,10 @@ import 'package:inno_entry/src/feature/entry/presentation/widgets/rectangle_stri
 
 void main() {
   group('Rectangle stripe tests:', () {
+    final size = Size(200, 1000);
+    double gap = 5;
     // get stripe coordinates
-    final stripeCoordinates = RectangleStripePainter.stripes(size: Size(200000, 100), gap: 5);
+    final stripeCoordinates = RectangleStripePainter.stripes(size: size, gap: gap);
 
     test("List is not empty.", () {
       expect(true, stripeCoordinates.isNotEmpty);
@@ -24,11 +26,16 @@ void main() {
 
     test("Stripes should be parallel to each other", () {
       bool isParellerAndEqualGaps() {
-        if(stripeCoordinates.isEmpty) return false;
         final slope = (stripeCoordinates[0].top.dy - stripeCoordinates[0].bottom.dy) / (stripeCoordinates[0].top.dx - stripeCoordinates[0].bottom.dx);
           for(int index = 1; index < stripeCoordinates.length; index++) {
             final thisSlope = (stripeCoordinates[0].top.dy - stripeCoordinates[0].bottom.dy) / (stripeCoordinates[0].top.dx - stripeCoordinates[0].bottom.dx);
-            if(thisSlope != slope) return false;
+            if(thisSlope != slope) {
+              debugPrint('''
+              slope : $slope,
+              thisSlope: $thisSlope
+              ''');
+              return false;
+            }
             //if(thisDistanceVector != distanceVector) return false;
           }
           return true;
@@ -36,6 +43,25 @@ void main() {
 
       expect(true, isParellerAndEqualGaps());
       
+    });
+
+    test("No side gaps inside the rectangle of the stripes", (){
+      bool yes = true;
+
+      for(int index = 0; index < stripeCoordinates.length; index++) {
+        final coordinate = stripeCoordinates[index];
+
+        if((coordinate.top.dx >= size.width || coordinate.top.dy <= 0) 
+          && 
+          (coordinate.bottom.dx <= 0 || coordinate.bottom.dy >= size.height)
+        ) {
+          continue;
+        }else{
+          debugPrint("flagged at: ${coordinate.toString()}");
+          yes = false; break;
+        }
+      }
+      expect(true, yes);
     });
   });
 }
